b86a27225fe4cea5784f7aa65e5bc4dfe9fe7dfc
anwarkvzk <anvarvzk521@gmail.com>
2024-04-09 11:55:47 +0530

    created user request and connected the backend port

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,19 +1,23 @@
-import { Auth0Provider, User } from "@auth0/auth0-react";
-import AppState from "@auth0/auth0-react";
+import { useCreateMyUser } from "@/api/MyUserApi";
+import { Auth0Provider, User,AppState } from "@auth0/auth0-react";
 
 type Props = {
   children: React.ReactNode;
 };
-const onRedirectCallback = (appState?: AppState, user?: User) => {
-  console.log("USER", user);
-};
+
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const { createUser } = useCreateMyUser();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
   if (!domain || !clientId || !redirectUri) {
     throw new Error("Unable to Initialise auth");
   }
+  const onRedirectCallback = (appState?: AppState, user?: User) => {
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
+  }; 
   return (
     <Auth0Provider
       domain={domain}
