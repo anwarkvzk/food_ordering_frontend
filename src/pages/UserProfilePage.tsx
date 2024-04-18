2324d25bfc9a13db412be92a8c1e6fa7ccf71fba
anwarkvzk <anvarvzk521@gmail.com>
2024-04-18 22:39:37 +0530

    Get user API

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,9 +1,14 @@
-import { useUpdaeMyUser } from "@/api/MyUserApi";
+import { useGetMyUser, useUpdaeMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 const UserProfilePage = () => {
-  const { updateUser, isLoading } = useUpdaeMyUser();
-  return <UserProfileForm onSave={updateUser} isLoading={isLoading} />;
+  const { currentUser, isLoading: isGetLoading } = useGetMyUser();
+  const { updateUser, isLoading: isUpdateLoading } = useUpdaeMyUser();
+
+  if (isGetLoading) {
+    return <span>Loading...</span>;
+  }
+  return <UserProfileForm onSave={updateUser} isLoading={isUpdateLoading} />;
 };
 
 export default UserProfilePage;
