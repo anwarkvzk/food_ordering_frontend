465172ccc188792ef7d5d5a379b572b067af47e3
anwarkvzk <anvarvzk521@gmail.com>
2024-04-22 19:25:43 +0530

    Pre-populate restaurant form and refershing managefrom issue fixed

--- src/forms/manage-restaurant-form/ImageSection.tsx
+++ src/forms/manage-restaurant-form/ImageSection.tsx
@@ -1,3 +1,4 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import {
   FormControl,
   FormDescription,
@@ -9,7 +10,8 @@ import { Input } from "@/components/ui/input";
 import { useFormContext } from "react-hook-form";
 
 const ImageSection = () => {
-  const { control } = useFormContext();
+  const { control, watch } = useFormContext();
+  const exisitngImageUrl = watch("imageUrl");
   return (
     <div className="space-y-2">
       <div>
@@ -19,7 +21,15 @@ const ImageSection = () => {
           search results. Adding a new image will overwrite the exisitng one.
         </FormDescription>
       </div>
-      <div className="flex flex-col gap-8 w-[50%]">
+      <div className="flex flex-col gap-8 md:w-[50%]">
+        {exisitngImageUrl && (
+          <AspectRatio ratio={16 / 9}>
+            <img
+              src={exisitngImageUrl}
+              className="rounded-md object-cover h-full w-full"
+            />
+          </AspectRatio>
+        )}
         <FormField
           control={control}
           name="imageFile"
