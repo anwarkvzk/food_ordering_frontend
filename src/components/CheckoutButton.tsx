6d1c31cd2a1bd20cb578ae2cbf0121b8ca62768e
anwarkvzk <anvarvzk521@gmail.com>
2024-04-27 15:45:26 +0530

    confirm delivery details

--- src/components/CheckoutButton.tsx
+++ src/components/CheckoutButton.tsx
@@ -2,14 +2,26 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useLocation } from "react-router-dom";
 import { Button } from "./ui/button";
 import LodingButton from "./LodingButton";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import UserProfileForm, {
+  UserFormData,
+} from "@/forms/user-profile-form/UserProfileForm";
+import { useGetMyUser } from "@/api/MyUserApi";
 
-const CheckoutButton = () => {
+type Props = {
+  onCheckout: (userFormData: UserFormData) => void;
+  disabled: boolean;
+};
+
+const   CheckoutButton = ({ onCheckout, disabled }: Props) => {
   const {
     isAuthenticated,
     isLoading: isAuthLoading,
     loginWithRedirect,
   } = useAuth0();
   const { pathname } = useLocation();
+
+  const { currentUser, isLoading: isGetUserLoading } = useGetMyUser();
 
   const onLogin = async () => {
     await loginWithRedirect({
@@ -19,16 +31,32 @@ const CheckoutButton = () => {
     });
   };
 
-  if (!isAuthenticated) {
+  if (!isAuthenticated || !currentUser) {
     return (
       <Button onClick={onLogin} className="bg-orange-500 flex-1">
         Login In to Checkout
       </Button>
     );
   }
-  if(isAuthLoading){
+  if (isAuthLoading) {
     return <LodingButton />;
   }
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button disabled={disabled} className="bg-orange-500 flex-1">
+          Go to Checkout
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-[425px] md:min-w-[700px] bg-gray-50">
+        <UserProfileForm
+          currentUser={currentUser}
+          onSave={onCheckout}
+          isLoading={isGetUserLoading}
+        />
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default CheckoutButton;
