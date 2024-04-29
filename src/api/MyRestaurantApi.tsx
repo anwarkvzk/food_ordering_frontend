2db16536fea341d5ca04a2509693d8cf236dd3db
anwarkvzk <anvarvzk521@gmail.com>
2024-04-29 16:44:34 +0530

    restaurant order api handling

--- src/api/MyRestaurantApi.tsx
+++ src/api/MyRestaurantApi.tsx
@@ -2,6 +2,7 @@ import { Restaurant } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
+import { Order } from "@/types";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -88,13 +89,36 @@ export const useUpdateMyRestaurant = () => {
     isSuccess,
   } = useMutation(updateRestaurantRequest);
 
-  if(isSuccess){
-    toast.success("Restaurant Updated")
+  if (isSuccess) {
+    toast.success("Restaurant Updated");
   }
-  if(error){
-    toast.error("Unable to update restaurant")
+  if (error) {
+    toast.error("Unable to update restaurant");
   }
   return {
-    updateRestaurant, isLoading
-  }
+    updateRestaurant,
+    isLoading,
+  };
+};
+
+export const useGetMyRestaurantOrders = () => {
+  const { getAccessTokenSilently } = useAuth0();
+  const getMyRestaurantOrdersRequest = async (): Promise<Order[]> => {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/api/my/restaurant/order`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Failed to fetch orders");
+    }
+    return response.json();
+  };
+  const { data: orders, isLoading } = useQuery(
+    "fetchMyRestaurantOrders",
+    getMyRestaurantOrdersRequest
+  );
+  return {orders, isLoading }
 };
